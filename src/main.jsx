306204de5c965a49e05953fe9736971e044a703c
Emmanuel <emmanuelabona@gmail.com>
2025-05-29 17:42:25 -0400

    Edit  confid files and redume form

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,8 +12,12 @@ createRoot(document.getElementById('root')).render(
       redirectUri={window.location.origin + '/callback'}
       cacheLocation="localstorage"
       audience={import.meta.env.VITE_API_AUDIENCE}
-      >
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_API_AUDIENCE, // Explicitly set audience here
+      }}
+    >
       <App />
     </Auth0Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
